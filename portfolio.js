b60d193b7d788aadc0b230709b62850ed40ce850
harsh2911-jpg <harshmaheshwari690@gmail.com>
2025-09-09 11:13:04 +0530

    added portfolio.js

---
+++ portfolio.js
@@ -0,0 +1,11 @@
+
+document.addEventListener("DOMContentLoaded", () => {
+    const form = document.querySelector(".contact-form");
+  
+    form.addEventListener("submit", function (e) {
+      e.preventDefault();
+      alert("Thank you! Your message has been sent.");
+      form.reset();
+    });
+  });
+  
